c434965af0a49f92d7bf15090f8287bbc47f01f2
Nrupesh Surya <50907680+nrupeshsurya@users.noreply.github.com>
2020-05-03 13:02:56 +0530

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,3 +1,4 @@
 call C:/%enter your anaconda directory here%/Anaconda3/Scripts/activate
+cd to your project directory
 C:/%enter your anaconda directory here%/Anaconda3/python.exe "%enter your directory here%"
 
